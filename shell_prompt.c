0d122657be6a30731cbc1594bb5317ad9b560cfd
Astrokojo <astrokojo@gmail.com>
2023-09-21 21:15:27 +0000

    Update shell_prompt.c

--- shell_prompt.c
+++ shell_prompt.c
@@ -7,7 +7,7 @@
  *
  * Return: 0 on success, 1 on error, or error code
  */
-int print_shell(info_t *info, char **av)
+int print_shell(shell_info_t *info, char **av)
 {
 	ssize_t sh = 0;
 	int builtin_cmd = 0;
@@ -52,7 +52,7 @@ int print_shell(info_t *info, char **av)
  * 1 if builtin found but not successful,
  * -2 if builtin signals exit()
  */
-int find_builtin(info_t *info)
+int find_builtin(shell_info_t *info)
 {
 	int i, builtin_sig = -1;
 	builtin_tbl table[] = {
@@ -82,7 +82,7 @@ int find_builtin(info_t *info)
  * @info: the parameter
  * Return: the info struct
  */
-void cmd_path(info_t *info)
+void cmd_path(shell_info_t *info)
 {
 	char *path = NULL;
 	int element, key;
@@ -124,7 +124,7 @@ void cmd_path(info_t *info)
  *
  * Return: void
  */
-void fork_pid(info_t *info)
+void fork_pid(shell_info_t *info)
 {
 	pid_t _pid;
 
