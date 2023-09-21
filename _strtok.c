97ba74c4c25ac27f4eb77b51d206afa5b37c5706
Astrokojo <astrokojo@gmail.com>
2023-09-21 19:52:15 +0000

    Update _strtok.c

--- _strtok.c
+++ _strtok.c
@@ -1,17 +1,5 @@
 #include "header.h"
-/**
- *chk_delim - checks if character is counter1 delimeter
- *@c: the char to check
- *@delim: the delimeter string
- *Return: 1 if true, 0 if false
- */
-int chk_delim(char c, char *delim)
-{
-	while (*delim)
-		if (*delim++ == c)
-			return (1);
-	return (0);
-}
+
 
 /**
  * **_strtok - splits counter1 string into counter3
