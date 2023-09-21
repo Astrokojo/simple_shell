3cc22912221e0507e478c2da71cb06b8c89b562b
Astrokojo <astrokojo@gmail.com>
2023-09-21 20:13:59 +0000

    Update str_prototypes.c

--- str_prototypes.c
+++ str_prototypes.c
@@ -61,3 +61,19 @@ char *_strchr(char *str, char c)
 
 	return (NULL);
 }
+
+/**
+ * _strstr - locates a substring
+ * @haystack: stack to search through
+ * @needle: the substring to match
+ *
+ * Return: address of next char of haystack or NULL
+ */
+char *_strstr(const char *haystack, const  char *needle)
+{
+	while (*needle)
+		if (*needle++ != *haystack++)
+			return (NULL);
+
+	return ((char *)haystack);
+}
