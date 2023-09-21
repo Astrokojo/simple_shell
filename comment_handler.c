dfed69d4d9456913f81e7e05149318e4cc729710
Astrokojo <astrokojo@gmail.com>
2023-09-21 19:46:35 +0000

    Add files via upload

---
+++ comment_handler.c
@@ -0,0 +1,16 @@
+/**
+ * remove_comment - function handles #
+ * @buf: string buffer
+ *
+ * Return: Always 0;
+ */
+void remove_comment(char *buf)
+{
+	int hash;
+
+	for (hash = 0; buf[hash] != '\0'; hash++)
+		if (buf[hash] == '#' && (!hash || buf[has - 1] == ' '))
+		{
+			buf[hash] = '\0';
+				break;
+		}
