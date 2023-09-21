55ffbe615c87802a860f8702f6e712521d83b76a
Astrokojo <astrokojo@gmail.com>
2023-09-21 16:05:32 +0000

    Update header.h

--- header.h
+++ header.h
@@ -1,16 +1,16 @@
 #ifndef _SHELL_H_
 #define _SHELL_H_
 
-#include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
-#include <string.h>
+#include <stdio.h>
 #include <sys/types.h>
-#include <sys/wait.h>
-#include <sys/stat.h>
+#include <string.h>
+#include <stdlib.h>
 #include <limits.h>
 #include <fcntl.h>
 #include <errno.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
 
 #define BUF_SIZE 1024
 #define BUF_FLUSH -1
