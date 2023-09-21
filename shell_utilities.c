695e158f2e59bb7f924c867c968a9f197e067b1e
Astrokojo <astrokojo@gmail.com>
2023-09-21 19:45:42 +0000

    Update shell_utilities.c

--- shell_utilities.c
+++ shell_utilities.c
@@ -1,23 +1,5 @@
 #include "header.h"
 /**
- *remove_comments -handles the hashtag(#) in comments
- *@buf: address of the string to modify
- *
- * Return: Always 0;
- */
-void remove_comments(char *buf)
-{
-	int comment;
-
-	for (comment = 0; buf[comment] != '\0'; comment++)
-		if (buf[comment] == '#' && (!comment || buf[comment - 1] == ' '))
-		{
-			buf[comment] = '\0';
-			break;
-		}
-}
-
-/**
  * shell_hist - print the shell history
  * @shell_info: inpit
  * Return: 0 Success
