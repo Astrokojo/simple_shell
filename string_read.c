799414b4e42a2ca9498829b507c407f8d27fc34e
Astrokojo <astrokojo@gmail.com>
2023-09-21 20:15:45 +0000

    Update and rename str_prototypes.c to string_read.c

--- string_read.c
+++ string_read.c
@@ -15,33 +15,6 @@ int _strlen(char *str)
 	return (i);
 }
 
-/**
- *_strdup - duplicates a string
- *@str: a string parameter
- *
- *Return: NULL or a pointer
- */
-char *_strdup(const char *str)
-{
-	char *dup;
-	int length = 0;
-
-	if (str == NULL)
-		return (NULL);
-
-	while (*str++)
-		length++;
-
-	dup = malloc(sizeof(char) * (length + 1));
-
-	if (!dup)
-		return (NULL);
-
-	for (length++; length--;)
-		dup[length] = *--str;
-
-	return (dup);
-}
 
 
 /**
