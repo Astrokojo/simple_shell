792a045d0fc0d552e66ea4e54cf628731ba77d50
Astrokojo <astrokojo@gmail.com>
2023-09-21 20:10:55 +0000

    Update fstr_prototypes.c

--- fstr_prototypes.c
+++ fstr_prototypes.c
@@ -71,3 +71,25 @@ char *_strncat(char *dest, char *src, int num)
 	dest[i + j] = '\0';
 	return (dest);
 }
+
+/**
+ * _strcmp - compares two strings
+ * @s1: the first str
+ * @s2: the second str
+ *
+ * Return: negative if s1 < s2, positive if s1 > s2, zero if s1 == s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2)
+	{
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+		s1++;
+		s2++;
+	}
+	if (*s1 == *s2)
+		return (0);
+	else
+		return (*s1 < *s2 ? -1 : 1);
+}
