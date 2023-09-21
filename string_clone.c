500829469bf48d035e3de1d328cc02ef574c648b
Astrokojo <astrokojo@gmail.com>
2023-09-21 20:22:41 +0000

    Update string_clone.c

--- string_clone.c
+++ string_clone.c
@@ -26,3 +26,38 @@ char *_strdup(const char *str)
 
 	return (dup);
 }
+
+/**
+ * _strcpy - copies a string from src to dest
+ * @src: the string source
+ * @dest: string destination
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i]; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
+
+	return (dest);
+}
+
+/**
+ *_strncpy - copies a string
+ *@src: the source string
+ *@dest: the destination string
+ *@num: num of bytes to copy
+ *Return: returns pointer to resulting string dest
+ */
+char *_strncpy(char *dest, char *src, int num)
+{
+	int i;
+
+	for (i = 0; src[i] && i < num; i++)
+		dest[i] = src[i];
+	for ( ; i < num; i++)
+		dest[i] = '\0';
+	return (dest);
+}
