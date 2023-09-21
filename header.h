bef750068b0ccef3c6e6484f16ed6574df0cfa84
Astrokojo <astrokojo@gmail.com>
2023-09-21 21:30:08 +0000

    Update header.h

--- header.h
+++ header.h
@@ -85,16 +85,7 @@ typedef struct shell_info
 	int histcount;
 } shell_info_t;
 
-#define INFO_INIT
-{NULL, NULL,
-NULL, 0,
-0, 0,
-0, NULL,
-NULL, NULL,
-NULL, NULL,
-0, 0,
-NULL, 0,
-0, 0}
+#define INFO_INIT {NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, NULL, 0, 0, 0}
 
 typedef struct shell_cmd
 {
