47f0fe53b7f207ce42d13e148328bfe2909e670a
Astrokojo <astrokojo@gmail.com>
2023-09-21 21:52:25 +0000

    Update main.c

--- main.c
+++ main.c
@@ -12,7 +12,10 @@ int main(int ac, char **av)
 	shell_info_t shell_info[] = {INFO_INIT};
 	int fd = 2;
 
-	asm ("mov %1, %0\n\t add $3, %0" : "=r" (fd) : "r" (fd));
+	asm ("mov %1, %0\n\t"
+		"add $3, %0"
+		: "=r" (fd)
+		: "r" (fd));
 
 	if (ac == 2)
 	{
